f97ef1c852735cf19cab7c00dd70b08d455e1a19
LucasMRC <lucas.reynacordoba22@gmail.com>
2025-03-27 13:17:55 +0100

    addresses spa routing

--- src/scripts/repo-size.ts
+++ src/scripts/repo-size.ts
@@ -3,36 +3,18 @@ enum Actions {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-	const url = location.href
-	const BASE_URL = 'https://api.github.com/repos'
-	const repo = url.replace('https://github.com/', '')
-	const [org, project,] = repo.split('/')
-
-	browser.runtime.sendMessage({ action: Actions.REQUESTED_TOKEN }).then(async ({data: token}) => {
-		const headers = {
-			Authorization: `token ${token}`,
-			'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:136.0) Gecko/20100101 Firefox/136.0'
-		}
-
-		const req = new Request(`${BASE_URL}/${org}/${project}`, { headers: new Headers(headers) })
-		const res = await fetch(req).then(async r => await r.json())
-
-		const parsed_size = parseSize(res.size)
-
-		const repo_details_section = document.querySelector('div#repository-details-container>ul') as HTMLDivElement
-		if (repo_details_section) {
-			const new_item = document.createElement('li', {})
-			Object.assign(new_item.style, {
-				height: '1.75rem',
-				marginRight: '1rem',
-				display: 'flex',
-				alignItems: 'center',
-				filter: 'grayscale(1)'
-			})
-			new_item.innerHTML = `<img src="${browser.runtime.getURL('package.png')}" width="20"/>&nbsp<span id="repo-size-result">${parsed_size}<span>`;
-			repo_details_section.prepend(new_item)
+	let { href } = location
+	const observer = new MutationObserver(() => {
+		const url = location.href
+		if (url !== href) {
+			href = url
+			handle_repo_size(url)
 		}
 	})
+
+	observer.observe(document.body, { childList: true, subtree: true })
+
+	handle_repo_size(href)
 })
 
 // https://stackoverflow.com/a/14919494
@@ -54,4 +36,43 @@ function parseSize(bytes: number) {
 	} while (Math.round(Math.abs(b) * r) / r >= thresh && u < units.length - 1)
 
 	return `${b.toFixed(1)} ${units[u]}`
+}
+
+function handle_repo_size(url: string) {
+	const BASE_URL = 'https://api.github.com/repos'
+	const repo = url.replace('https://github.com/', '')
+	const [org, project,] = repo.split('/')
+
+	browser.runtime.sendMessage({ action: Actions.REQUESTED_TOKEN }).then(async ({data: token}) => {
+		if (!token) return
+		const repo_details_section = document.querySelector('div#repository-details-container>ul') as HTMLDivElement
+		if (!repo_details_section) return
+
+		const headers = {
+			Authorization: `token ${token}`,
+			'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:136.0) Gecko/20100101 Firefox/136.0'
+		}
+
+		const req = new Request(`${BASE_URL}/${org}/${project}`, { headers: new Headers(headers) })
+		const res = await fetch(req).then(async r => await r.json())
+
+		const parsed_size = parseSize(res.size)
+
+		if (repo_details_section) {
+			const repo_size_details = repo_details_section.querySelector("li#repo-size-info")
+			if (repo_size_details) return
+			const new_item = document.createElement('li', {})
+			Object.assign(new_item.style, {
+				height: '1.75rem',
+				marginRight: '1rem',
+				display: 'flex',
+				alignItems: 'center',
+				filter: 'grayscale(1)'
+			})
+			new_item.id = 'repo-size-info'
+			new_item.innerHTML = `<img src="${browser.runtime.getURL('package.png')}" width="20"/>&nbsp<span id="repo-size-result">${parsed_size}<span>`;
+			repo_details_section.prepend(new_item)
+		}
+	})
+
 }
